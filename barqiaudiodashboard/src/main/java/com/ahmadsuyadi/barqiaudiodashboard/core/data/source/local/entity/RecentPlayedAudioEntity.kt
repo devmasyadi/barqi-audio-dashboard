4e69b6baa8dee40ca5f77cc94f64da1f5ac1bed1
devmasyadi <devmasyadi@yahoo.com>
2021-01-06 10:48:36 +0700

    fix primaryKey

--- barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/data/source/local/entity/RecentPlayedAudioEntity.kt
+++ barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/data/source/local/entity/RecentPlayedAudioEntity.kt
@@ -10,7 +10,7 @@ data class RecentPlayedAudioEntity(
     @PrimaryKey
     @NonNull
     @ColumnInfo(name = "id")
-    val id: String? = null,
+    val id: String? = "0",
 
     @ColumnInfo(name = "duration")
     val duration: String? = null,
