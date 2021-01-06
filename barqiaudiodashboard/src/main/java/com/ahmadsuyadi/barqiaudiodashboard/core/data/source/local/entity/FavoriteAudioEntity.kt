42a6cebb67db9b5087622a60f00b1ef88b5cff34
devmasyadi <devmasyadi@yahoo.com>
2021-01-06 10:07:24 +0700

    add @NonNull primaryKey database

--- barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/data/source/local/entity/FavoriteAudioEntity.kt
+++ barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/data/source/local/entity/FavoriteAudioEntity.kt
@@ -1,5 +1,6 @@
 package com.ahmadsuyadi.barqiaudiodashboard.core.data.source.local.entity
 
+import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
@@ -7,6 +8,7 @@ import androidx.room.PrimaryKey
 @Entity(tableName = "favoriteAudio")
 data class FavoriteAudioEntity(
     @PrimaryKey
+    @NonNull
     @ColumnInfo(name = "_id")
     val id: String? = null,
 
