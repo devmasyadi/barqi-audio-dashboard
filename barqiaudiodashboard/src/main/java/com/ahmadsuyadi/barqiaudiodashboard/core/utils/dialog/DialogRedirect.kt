6075e70b7d32a6d14f376c64361cd70cfea99af7
devmasyadi <devmasyadi@yahoo.com>
2021-01-12 08:08:37 +0700

    change checkRedirectApp

--- barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/utils/dialog/DialogRedirect.kt
+++ barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/utils/dialog/DialogRedirect.kt
@@ -4,17 +4,18 @@ import android.content.Context
 import com.ahmadsuyadi.barqiaudiodashboard.R
 import com.ahmadsuyadi.barqiaudiodashboard.core.utils.ConfigBarqiAudioDashboard
 import com.ahmadsuyadi.barqiaudiodashboard.core.utils.extesion.gotoUrl
+import com.ahmadsuyadi.barqiaudiodashboard.core.utils.extesion.toTextHtml
 import org.jetbrains.anko.alert
 
 fun Context.checkRedirectApp(message: String?) {
     with(ConfigBarqiAudioDashboard) {
         if (isOnRedirect && urlRedirect.isNotEmpty()) {
             alert(
-                    message ?: getString(R.string.msg_update_redirect),
+                    message ?: getString(R.string.msg_update_redirect).toTextHtml().toString(),
                     getString(R.string.update_info)
             ) {
-                negativeButton(getString(R.string.no)) {}
-                positiveButton(getString(R.string.ok)) {
+                negativeButton(getString(R.string.later)) {}
+                positiveButton(getString(R.string.update)) {
                     urlRedirect.gotoUrl(this@checkRedirectApp)
                 }
                 isCancelable = false
--- barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/utils/extesion/String+Ext.kt
+++ barqiaudiodashboard/src/main/java/com/ahmadsuyadi/barqiaudiodashboard/core/utils/extesion/String+Ext.kt
@@ -3,9 +3,19 @@ package com.ahmadsuyadi.barqiaudiodashboard.core.utils.extesion
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
+import android.text.Html
+import android.text.Spanned
 
 fun String.gotoUrl(context: Context) {
     val uri = Uri.parse(this)
     val intent = Intent(Intent.ACTION_VIEW, uri)
     context.startActivity(intent)
+}
+
+fun String.toTextHtml(): Spanned? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N)
+        Html.fromHtml(this, Html.FROM_HTML_MODE_COMPACT)
+    else
+        Html.fromHtml(this)
 }
